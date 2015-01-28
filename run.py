423800eb113ccfbb8d837e76b992ce11467c6c89
Pauli Virtanen <pav@iki.fi>
2015-01-28 02:09:32 +0200

    Make ccache actually work properly

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import os
 import sys
 import argparse
 import subprocess
+import sysconfig
 
 
 EXTRA_PATH = ['/usr/lib/ccache', '/usr/lib/f90cache',
@@ -24,7 +25,29 @@ def run_asv(*args):
     cwd = os.path.abspath(os.path.dirname(__file__))
     env = dict(os.environ)
     env['PATH'] = os.pathsep.join(EXTRA_PATH + env.get('PATH', '').split(os.pathsep))
+
+    # Required to make ccache work properly --- pip builds and installs stuff
+    # to paths with random components in directory names, which confuses ccache.
+    #
+    # To work around this, we need to encourage ccache to be sloppy, and to
+    # remove the -g flag from compiler flags --- it causes file names to be
+    # embedded to the object files, which ccache does not like.
+    env['CCACHE_SLOPPINESS'] = 'file_macro,time_macros'
+    env['CCACHE_UNIFY'] = '1'
+    env['CFLAGS'] = drop_g_flag(sysconfig.get_config_var('CFLAGS'))
+    env['OPT'] = drop_g_flag(sysconfig.get_config_var('OPT'))
+    env['LDSHARED'] = drop_g_flag(sysconfig.get_config_var('LDSHARED'))
+
     return subprocess.call(cmd, env=env, cwd=cwd)
+
+
+def drop_g_flag(flags):
+    """
+    Drop -g from command line flags
+    """
+    if not flags:
+        return flags
+    return " ".join(x for x in flags.split() if x not in ('-g', '-g1', '-g2', '-g3'))
 
 
 if __name__ == "__main__":
